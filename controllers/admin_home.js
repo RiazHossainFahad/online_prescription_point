365eb69a07f4f0b8b852039d6a54ef7597acb679
RiazHossainFahad <mdfahad6562@gmail.com>
2019-03-04 14:30:57 +0600

    delete and edit other user   added for admin.

--- controllers/admin_home.js
+++ controllers/admin_home.js
@@ -61,6 +61,75 @@ router.get('/user_list',(req, res) => {
   };
   res.render('admin/user_list', data);
   });
-
 });
+
+router.get('/edit/:id', function(req, res){
+
+ userModel.get(req.params.id, (result_info) => {
+  if(result_info != ""){
+     //info from additional_info table 
+   userModel.get_additional(req.params.id, (result_add_info) => {
+   if(result_add_info != ""){
+    var data = {
+     user_info:  result_info,
+     add_info:   result_add_info
+    }
+    res.render('admin/edit_user_profile',data); //render edit_profile view with the data
+  }
+
+  else{
+   res.redirect('/home-admin/user_list');
+  }
+ });
+}
+else{
+ res.redirect('/home-admin/user_list');
+}
+});
+});
+
+router.post('/edit/:id',(req, res) => {
+    
+ var update_user = {
+   name:                 req.body.name,
+   u_email:              req.body.u_email,
+   user_type:            req.body.user_type,
+   relationship_status:  req.body.relationship_status,
+   u_pass:               req.body.u_pass,
+   u_location:           req.body.u_location,
+   u_gender:             req.body.u_gender,
+   u_birthday:           req.body.u_birthday,
+   user_id:              req.params.id
+ };
+
+  //update user_info table information
+ userModel.update(update_user, (user_update_status)=>{
+  if(user_update_status){
+ 
+   //update additional_info table info
+  var additional = {
+   hospital_name: req.body.hospital_name,
+   u_degree:      req.body.u_degree,
+   user_lic_no:   req.body.user_lic_no,
+   user_id:       req.params.id
+  };
+ 
+  userModel.update_additional(additional,(user_add_update_status)=>{
+
+   if(user_add_update_status){
+    res.redirect('/home-admin/user_list');
+   }
+  });
+  }
+ });
+});
+
+router.get("/delete/:id", function(req, res){
+
+	adminModel.delete(req.params.id, function(status){
+			res.redirect('/home-admin/user_list');
+	});
+});
+
+
 module.exports = router;
--- model/admin-model.js
+++ model/admin-model.js
@@ -2,4 +2,21 @@ var db = require('./db');
 
 
 module.exports = {
+ delete: function(id, callback){
+		var sql = "delete from users_info where user_id=?";
+		db.execute(sql, [id], function(status){
+			if(status){
+    var sql = "delete from additional_info where user_id=?";
+    db.execute(sql, [id], function(status_add){
+     if(status_add){
+      callback(true);
+     }else{
+      callback(false);
+     }
+    });
+			}else{
+				callback(false);
+			}
+		});
+	}
 };
